0e3ecb2c90b157fea18afd9d8313a9e6ddb0d8fe
kurloal <kurloal_aehit@students.vizja.pl>
2025-06-11 20:45:21 +0300

    Rename server.js to server.cjs to CommonJS

---
+++ MovieApp/server.cjs
@@ -0,0 +1,46 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+//import movieRoutes from './routes/movieRoutes.js';
+//import listRoutes from './routes/listRoutes.js';
+import db from "./database.js";
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+dotenv.config();
+
+const { mongoConnect } = db;
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const apiRoutes = require('./routes/api');
+app.use('/api', apiRoutes);
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use(cors());
+
+mongoConnect(async () => {
+  const { ensureDefaultLists } = await import('./controllers/listController.js');
+  await ensureDefaultLists();
+
+  const movieRoutes = (await import('./routes/movieRoutes.js')).default;
+  const listRoutes = (await import('./routes/listRoutes.js')).default;
+
+  app.use('/api/movies', movieRoutes);
+  app.use('/api/lists', listRoutes);
+
+  app.listen(PORT);
+
+  app.get('/add', (req, res) => {
+  res.render('add'); 
+});
+
+});

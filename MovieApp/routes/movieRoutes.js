1c3a9db0d18dd5a2358cc293608d9a4ba9a99145
incmrijdty <alesia.sichova@yandex.by>
2025-06-14 04:17:01 +0200

    back end 2nd api for movie details

--- MovieApp/routes/movieRoutes.js
+++ MovieApp/routes/movieRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 const router = Router();
-import { getMoviesView, getPopularMovies, searchMovies } from '../controllers/movieController.js';
+import { getMoviesView, getPopularMovies, searchMovies, getMergedMovieDetails } from '../controllers/movieController.js';
 
 router.get('/', getMoviesView);
 router.get('/popular', getPopularMovies);
 router.get('/search', searchMovies);
+router.get('/:tmdbId/details', getMergedMovieDetails);
 
 export default router;

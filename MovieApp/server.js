993aa76bbd716f75346520d69523aed244cb1053
incmrijdty <alesia.sichova@yandex.by>
2025-06-14 04:40:59 +0200

    adding swagger docs

--- MovieApp/server.js
+++ MovieApp/server.js
@@ -2,11 +2,11 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
-//import movieRoutes from './routes/movieRoutes.js';
-//import listRoutes from './routes/listRoutes.js';
 import db from "./database.js";
 import path from 'path';
 import { fileURLToPath } from 'url';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 
 dotenv.config();
 
@@ -15,6 +15,8 @@ const { mongoConnect } = db;
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+const swaggerDocument = YAML.load('./MovieApp/swagger.yaml');
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -26,15 +28,13 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cors());
 app.use(cookieParser());
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 mongoConnect(async () => {
 
   const movieRoutes = (await import('./routes/movieRoutes.js')).default;
   const listRoutes = (await import('./routes/listRoutes.js')).default;
   const authRoutes = (await import('./routes/authRoutes.js')).default;
-
-  app.get('/test', (req, res) => {
-    res.json({ message: 'Hello World' });
-  });
 
   app.use('/api/auth', authRoutes);
   app.use('/api/movies', movieRoutes);

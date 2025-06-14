f86e67752a7b50a96e869dcf90ac019b0c140da5
incmrijdty <alesia.sichova@yandex.by>
2025-06-14 04:57:53 +0200

    logging out added

--- MovieApp/routes/authRoutes.js
+++ MovieApp/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { login, register, showLoginForm, showRegisterForm } from '../controllers/authController.js';
+import { login, register, showLoginForm, showRegisterForm, logout } from '../controllers/authController.js';
 
 const router = express.Router();
 
@@ -7,5 +7,6 @@ router.get('/login', showLoginForm);
 router.get('/register', showRegisterForm);
 router.post('/register', register);
 router.post('/login', login);
+router.post('/logout', logout);
 
 export default router;
